9735d9a093f5c3dff0665e09295479a77330948e
alisonsousa05 <alisonsousa05@hotmail.com>
2024-07-11 15:50:09 -0300

    UI ready

--- script.js
+++ script.js
@@ -1,3 +1,104 @@
+const pedra = document.querySelector('button[name="pedra"]');
+const papel = document.querySelector("button[name='papel']");
+const tesoura = document.querySelector("button[name='tesoura']");
+const resultado = document.querySelector("p[name='resultado']");
+
+let humanScore=0, computerScore=0;
+function checaVencedor(){
+    if (humanScore==5){
+        resultado.textContent = "Você venceu!";
+        humanScore = 0;
+        computerScore = 0;
+        return;
+    }
+    else if(computerScore==5){
+        resultado.textContent = "Perdeu playboy! Computador venceu MUAHUAHUA";
+        humanScore = 0;
+        computerScore = 0;
+        return;
+    }
+}
+pedra.addEventListener("click", roundPedra);
+papel.addEventListener("click", roundPapel);
+tesoura.addEventListener("click", roundTesoura);
+
+function displayResult(){
+    humanScore+=1;
+    resultado.textContent = `Sua escolha é pedra. O computador escolheu tesoura. Você: ${humanScore} Computador: ${computerScore}`;
+    return;
+}
+
+/*
+pedra.addEventListener("click", playRound("pedra", getComputerChoice()));
+papel.addEventListener("click", playRound("papel", getComputerChoice()));
+tesoura.addEventListener("click", playRound("tesoura", getComputerChoice()));
+*/
+
+function roundPedra(){
+    opcaoComputador = getComputerChoice();
+    if (opcaoComputador==="scissors"){
+        humanScore += 1;
+        console.log("Human wins the round");
+        checaVencedor();
+        resultado.textContent = `Sua escolha é pedra. O computador escolheu tesoura. Você: ${humanScore} Computador: ${computerScore}`;
+        return;
+    }
+    else if (opcaoComputador==="rock"){
+        resultado.textContent = `Sua escolha é pedra. O computador escolheu pedra também. Você: ${humanScore} Computador: ${computerScore}`;
+        return;
+    }
+    else{
+        computerScore += 1;
+        resultado.textContent = `Sua escolha é pedra. O computador escolheu papel. Você: ${humanScore} Computador: ${computerScore}`;
+        console.log("Computer wins the round");
+        checaVencedor();
+        return;
+    }
+}
+
+function roundPapel(){
+    opcaoComputador = getComputerChoice();
+    if (opcaoComputador==="scissors"){
+        computerScore += 1;
+        console.log("Human wins the round");
+        checaVencedor();
+        resultado.textContent = `Sua escolha é papel. O computador escolheu tesoura. Você: ${humanScore} Computador: ${computerScore}`;
+        return;
+    }
+    else if (opcaoComputador==="rock"){
+        humanScore += 1;
+        resultado.textContent = `Sua escolha é papel. O computador escolheu pedra. Você: ${humanScore} Computador: ${computerScore}`;
+        checaVencedor();
+        return;
+    }
+    else{
+        resultado.textContent = `Sua escolha é papel. O computador escolheu papel também. Você: ${humanScore} Computador: ${computerScore}`;
+        console.log("Computer wins the round");
+        return;
+    }
+}
+
+function roundTesoura(){
+    opcaoComputador = getComputerChoice();
+    if (opcaoComputador==="scissors"){
+        console.log("Human wins the round");
+        resultado.textContent = `Sua escolha é tesoura. O computador escolheu tesoura também. Você: ${humanScore} Computador: ${computerScore}`;
+        return;
+    }
+    else if (opcaoComputador==="rock"){
+        computerScore += 1;
+        resultado.textContent = `Sua escolha é tesoura. O computador escolheu pedra. Você: ${humanScore} Computador: ${computerScore}`;
+        checaVencedor();
+        return;
+    }
+    else{
+        resultado.textContent = `Sua escolha é tesoura. O computador escolheu papel. Você: ${humanScore} Computador: ${computerScore}`;
+        humanScore += 1;
+        checaVencedor();
+        console.log("Computer wins the round");
+        return;
+    }
+}
 
 function getComputerChoice(){
     let number = Math.random();
@@ -12,9 +113,8 @@ function getComputerChoice(){
     }
     return choice;
 }
-
+/*
 function getHumanChoice(){
-    let humanChoice = prompt("What do you choose, mortal?").toLowerCase();
     while (humanChoice != "scissors" && humanChoice !="paper" && humanChoice !="rock"){
         humanChoice = prompt("WRONG!! You must choose either 'scissors' or 'rock' or 'paper'").toLowerCase();
         console.log(humanChoice);
@@ -22,6 +122,67 @@ function getHumanChoice(){
 
     return humanChoice; 
 }
+*/
+
+function playRound(humanChoice, computerChoice){
+    console.log(computerChoice);
+    if (humanChoice === "rock"){
+        if (computerChoice==="scissors"){
+            humanScore += 1;
+            console.log("Human wins the round");
+            resultado.textContent = `Sua escolha é pedra. O computador escolheu tesoura. Você: ${humanScore} Computador: ${computerScore}`;
+            return;
+        }
+        else if (computerChoice==="rock"){
+            resultado.textContent = `Sua escolha é pedra. O computador escolheu pedra também. Você: ${humanScore} Computador: ${computerScore}`;
+            return;
+        }
+        else{
+            computerScore += 1;
+            resultado.textContent = `Sua escolha é pedra. O computador escolheu papel. Você: ${humanScore} Computador: ${computerScore}`;
+            console.log("Computer wins the round");
+            return;
+        }
+    }
+    else if (humanChoice === "scissors"){
+        if (computerChoice === "paper"){
+            humanScore += 1;
+            resultado.textContent = `Sua escolha é tesoura. O computador escolheu papel. Você: ${humanScore} Computador: ${computerScore}`;
+            console.log("Human wins the round");
+            return;
+        }
+        else if(computerChoice==="scissors"){
+            resultado.textContent = `Sua escolha é tesoura. O computador escolheu tesoura também. Você: ${humanScore} Computador: ${computerScore}`;
+            return;
+        }
+        else{
+            computerScore += 1;
+            console.log("Computer wins the round");
+            resultado.textContent = `Sua escolha é tesoura. O computador escolheu pedra. Você: ${humanScore} Computador: ${computerScore}`;
+            return;
+        }
+    }
+    else if (humanChoice=="paper"){
+        if(computerChoice==="rock"){
+            humanScore +=1;
+            console.log("Human wins the round");
+            resultado.textContent = `Sua escolha é papel. O computador escolheu pedra. Você: ${humanScore} Computador: ${computerScore}`;
+            return;
+        }
+        else if(computerChoice==="paper"){
+            resultado.textContent = `Sua escolha é papel. O computador escolheu papel também. Você: ${humanScore} Computador: ${computerScore}`;
+            return;
+        }
+        else{
+            computerScore += 1;
+            resultado.textContent = `Sua escolha é papel. O computador escolheu tesoura. Você: ${humanScore} Computador: ${computerScore}`;
+            console.log("Computer wins the round");
+            return;
+        }
+    }
+}
+
+/*
 function playGame(){
     let humanScore=0
     computerScore=0;
@@ -81,6 +242,5 @@ function playGame(){
     playRound(getHumanChoice(), getComputerChoice());
 
 }
-playGame();
 
-
+*/
